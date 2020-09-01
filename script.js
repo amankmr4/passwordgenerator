7fd3a3fa28a2850f6468b2c97d34e5d8dd14c594
Aman Kumar <aman.kmr4@yahoo.com>
2020-09-02 00:08:53 +1000

    added for loop to run against char variable

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-// Assignment Code
+// Assignment Code bottom is for the button
 var generateBtn = document.querySelector("#generate");
-var uppercasePrompt = confirm("would you like uppercase characters?");
-var lowercasePrompt = confirm("would you like lowercase characters?");
-var numbersPrompt = confirm("would you like numbers?");
-var symbolsPrompt = confirm("would you like symbols?");
-var numbersOfCharacters = prompt("how many characters would you like in your password")
+// i have moved this from function writePassword 
+var passwordText = document.querySelector("#password");
+// i moved this fucntion here 
+var password = generatePassword();
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
 
 }
 
+console.log(generatePassword(12, char));
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//--------------------------------------------------------
+//writing fucntion for generate password
+function generatePassword (characterlength, characters) {
+  var characterlength = prompt("Please choose the length of your of your password?");
+  var symbols = confirm("Would you like any symbols?");
+  var uppercase = confirm("would you like upper case letters?");
+  var lower = confirm("would you like lower case letters?");
 
+  // this will go against cthe character length set in prompt and run it
+  var pwd = " ";
+  for(var i=0;i < characterlength;i++){
+    pwd += lowerCasedCharacters.charat(math.floor(math.random() * characters.length));
+  }
 
+  return pwd
+}
 
-
-
-
-
-
-
-
-
+//---------------------------------------------------
