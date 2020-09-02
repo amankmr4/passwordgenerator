e1bf991e93e787a30df264d989cd6afb18a94b08
Aman Kumar <aman.kmr4@yahoo.com>
2020-09-03 00:47:21 +1000

    added more styling and fucntionality

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-// Assignment Code bottom is for the button
-var generateBtn = document.querySelector("#generate");
-// i have moved this from function writePassword 
-var passwordText = document.querySelector("#password");
-// i moved this fucntion here 
-var password = generatePassword();
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-// Write password to the #password input
-function writePassword() {
- 
-  passwordText.value = password;
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+})
 
-}
-
-console.log(generatePassword(12, char));
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//--------------------------------------------------------
-//writing fucntion for generate password
-function generatePassword (characterlength, characters) {
-  var characterlength = prompt("Please choose the length of your of your password?");
-  var symbols = confirm("Would you like any symbols?");
-  var uppercase = confirm("would you like upper case letters?");
-  var lower = confirm("would you like lower case letters?");
-
-  // this will go against cthe character length set in prompt and run it
-  var pwd = " ";
-  for(var i=0;i < characterlength;i++){
-    pwd += lowerCasedCharacters.charat(math.floor(math.random() * characters.length));
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
   }
-
-  return pwd
+  return passwordCharacters.join('')
 }
 
-//---------------------------------------------------
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
