ad42d4ae851080d8cc04fb851825a69a38ffbe66
Aman Kumar <aman.kmr4@yahoo.com>
2020-09-03 22:00:29 +1000

    final full working webpage and also Readme files

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,3 +1,4 @@
+// Here we are targeting HTML Elements and putting them into constants
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
@@ -16,12 +17,14 @@ const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
 ).concat(
   arrayFromLowToHigh(123, 126)
 )
-
+// Here we are adding an event listener to the number toggler to run function(syncCharacterAmount) on input
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
+//Here we are adding an event listener to the number toggler to run a fucntion on submit to prevent it from submitting and refreshing our page
 form.addEventListener('submit', e => {
   e.preventDefault()
+  //here we are targetting the values from the HTML page and adding them to constants
   const characterAmount = characterAmountNumber.value
   const includeUppercase = includeUppercaseElement.checked
   const includeNumbers = includeNumbersElement.checked
@@ -29,6 +32,8 @@ form.addEventListener('submit', e => {
   const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
   passwordDisplay.innerText = password
 })
+
+//Here we will run our main function generatepassword and this will run generate our password
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
@@ -43,6 +48,8 @@ function generatePassword(characterAmount, includeUppercase, includeNumbers, inc
   }
   return passwordCharacters.join('')
 }
+
+//Here we are writing a function which will grab characters from the ASCII Chart and add them to an array
 
 function arrayFromLowToHigh(low, high) {
   const array = []
